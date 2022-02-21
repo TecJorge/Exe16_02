960419a858191a623e8ca4d9bb170992023e7c1e
JorgeSousa <tec.jorgesousa@outlook.com>
2022-02-21 15:54:01 +0000

    Exe Dia 16 Resolvido com ArrayList

--- 16_02/src/main/java/Exe2/Main.java
+++ 16_02/src/main/java/Exe2/Main.java
@@ -1,8 +1,10 @@
 package Exe2;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        Contribuinte[] arr = new Contribuinte[12];
+        ArrayList <Contribuinte> arr = new ArrayList<>();
         Reformado ref1 = new Reformado("Jose Pereira", "Rua da Esquerda", 500, 400);
         Reformado ref2 = new Reformado("Carlos Cruz", "Rua da Direita", 300, 120);
         TrabalhadorProprio tp1 = new TrabalhadorProprio("Marilio Costa", "Rua da Frente", 800, 350);
@@ -11,25 +13,25 @@ public class Main {
         TrabalhadorOutrem to2 = new TrabalhadorOutrem("Jose Rocha", "Rua de Tras", 300, 50);
         Desempregado d1 = new Desempregado("Rahim", "Rua do Norte", 650);
         Desempregado d2 = new Desempregado("Sterling", "Rua do Nordeste", 850);
-        arr[0] = ref1;
-        arr[1] = ref2;
-        arr[2] = tp1;
-        arr[3] = tp2;
-        arr[4] = to1;
-        arr[5] = to2;
-        arr[6] = d1;
-        arr[7] = d2;
-        for (int i = 0; arr[i] != null; i++) {
-            System.out.printf("%s e tem de pagar %.2f EUR de impostos\n", arr[i], arr[i].Impostos());
+        arr.add(ref1);
+        arr.add(ref2);
+        arr.add(tp1);
+        arr.add(tp2);
+        arr.add(to1);
+        arr.add(to2);
+        arr.add(d1);
+        arr.add(d2);
+        for (Contribuinte contribuinte:arr) {
+            System.out.printf("%s e tem de pagar %.2f EUR de impostos\n", contribuinte, contribuinte.Impostos());
         }
-        for (int i = 0; arr[i] != null; i++) {
-            if (arr[i] instanceof Desempregado)
-                System.out.printf("%s e tem de pagar %.2f EUR de impostos\n", arr[i], arr[i].Impostos());
+        for (Contribuinte contribuinte:arr) {
+            if (contribuinte instanceof Desempregado)
+                System.out.printf("%s e tem de pagar %.2f EUR de impostos\n", contribuinte, contribuinte.Impostos());
         }
         Desempregado.setPercentOR(15);
-        for (int i = 0; arr[i] != null; i++) {
-            if (arr[i] instanceof Desempregado)
-                System.out.printf("%s e tem de pagar %.2f EUR de impostos\n", arr[i], arr[i].Impostos());
+        for (Contribuinte contribuinte:arr) {
+            if (contribuinte instanceof Desempregado)
+                System.out.printf("%s e tem de pagar %.2f EUR de impostos\n", contribuinte,contribuinte.Impostos());
 
 
         }
