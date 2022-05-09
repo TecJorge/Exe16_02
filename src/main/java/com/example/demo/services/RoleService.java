ae229c9d18230b378dc83d60439260768f4f72c5
JorgeSousa <tec.jorgesousa@outlook.com>
2022-05-09 16:39:27 +0100

    Spring Maroscas

--- src/main/java/com/example/demo/services/RoleService.java
+++ src/main/java/com/example/demo/services/RoleService.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RoleService {
-    @Autowired
+        @Autowired
     private RoleRepository  roleRepository;
 
     public RoleService() {
